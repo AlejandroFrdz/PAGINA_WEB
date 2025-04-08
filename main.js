07d77a5f1dc24a9f47605da7c951a57b8ed6e888
Alejandro Fernandez <aferfra@teleco.upv.es>
2025-04-08 23:35:34 +0200

    Todo en main.js y ya funciona carga y raton

--- main.js
+++ main.js
@@ -1,31 +1,39 @@
 // ###############PUNTERO###############
 
 //Velocidad del puntero
-//VARIABLES
-let followSpeed = 0.1; //Ajustarlo
+let followSpeed = 0.1;
 
 const circle = document.getElementById("circuloCursor");
-let ratonX= window.innerWidth / 2;
-let ratonY= window.innerHeight / 2;
+
+let ratonX = window.innerWidth / 2;
+let ratonY = window.innerHeight / 2;
 let circuloX = ratonX;
 let circuloY = ratonY;
 
-//LÓGICA
-//Animación del puntero (POSICIÓN)
-
 document.addEventListener("mousemove", (e) => {
-    ratonX = e.clientX;
-    ratonY = e.clientY;
+  ratonX = e.clientX;
+  ratonY = e.clientY;
 });
 
-//Animación suave usando requestAnimationFrame
 function animate() {
-    //Suavidad del puntero
+  if (circle) {
     circuloX += (ratonX - circuloX) * followSpeed;
     circuloY += (ratonY - circuloY) * followSpeed;
     circle.style.transform = `translate(${circuloX}px, ${circuloY}px)`;
-    requestAnimationFrame(animate);
+  }
+  requestAnimationFrame(animate);
 }
 
-animate();
+animate(); // SIEMPRE se ejecuta (en todas las páginas)
 
+
+// ###############CARGA PÁGINAS###############
+
+if (window.location.pathname.includes("CARGADOR.html")) {
+  const params = new URLSearchParams(window.location.search);
+  const nextPage = params.get("to") || "Index.html";
+
+  setTimeout(() => {
+    window.location.href = nextPage;
+  }, 1600); // Duración de la animación
+}
